a113cef1017b3f64fe2118236dc6b40ce87f6d18
Carla Joseph <carlajseph@gmail.com>
2020-06-24 17:54:55 -0400

    Added props

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,54 @@ class App extends Component {
         <body>
           <Header />
           <main>
-            <Octocat catName="Octoqueer" imageName="Octoqueer" />
+            <Octocat
+              catName="Octoqueer"
+              imageName="https://octodex.github.com//images/Octoqueer.png"
+              link="https://octodex.github.com//octoqueer/"
+              catNumber="#150:"
+              authorLink="https://github.com/cameronfoxly"
+              authorImage="https://github.com/cameronfoxly.png"
+            />
+            <Octocat
+              catName="Terracottocat"
+              imageName="https://octodex.github.com//images/Terracottocat_Single.png"
+              link="https://octodex.github.com//terracottocat/"
+              catNumber="#149:"
+              authorLink="https://github.com/cameronfoxly"
+              authorImage="https://github.com/chubbmo.png"
+            />
+            <Octocat
+              catName="Daftpunktocat-Thomas"
+              imageName="https://octodex.github.com//images/daftpunktocat-thomas.gif"
+              link="https://octodex.github.com//daftpunktocat-thomas/"
+              catNumber="#101:"
+              authorLink="https://github.com/jeejkang"
+              authorImage="hhttps://github.com/jeejkang.png"
+            />
+            <Octocat
+              catName="Grinchtocat"
+              imageName="https://octodex.github.com//images/grinchtocat.gif"
+              link="https://octodex.https://octodex.github.com//grinchtocat/"
+              catNumber="#110:"
+              authorLink="https://github.com/tonyjaramillo"
+              authorImage="https://github.com/tonyjaramillo.png"
+            />
+            <Octocat
+              catName="DoctocatBrown"
+              imageName="https://octodex.github.com//images/doctocat-brown.jpg"
+              link="https://octodex.github.com//doctocat-brown/"
+              catNumber="#67:"
+              authorLink="hhttps://github.com/jonrohan"
+              authorImage="https://github.com/jonrohan.png"
+            />
+            <Octocat
+              catName="Minertocat"
+              imageName="https://octodex.github.com//images/minertocat.png"
+              link="https://octodex.github.com//minertocat/"
+              catNumber="#1:"
+              authorLink="https://github.com/jeejkang"
+              authorImage="https://github.com/jeejkang.png"
+            />
           </main>
           <footer>© 2013-2020 GitHub, Inc. All rights reserved.</footer>
         </body>
