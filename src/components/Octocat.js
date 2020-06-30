90e45b3dd2fc2a41c9d95492deff5e23c7cf02bf
Carla Joseph <carlajseph@gmail.com>
2020-06-30 16:45:08 -0400

    Used props for cats

--- src/components/Octocat.js
+++ src/components/Octocat.js
@@ -1,46 +1,27 @@
 import React, { Component } from 'react'
 
 export default class Octocat extends Component {
-
   render() {
     return (
       <section>
-        <a className="octo" href="https://octodex.github.com//octoqueer/">
+        <a className="octo" href={this.props.link}>
           <img
-            src="https://octodex.github.com//images/Octoqueer.png"
-            alt="Octoqueer"
+            src={this.props.imageName}
+            alt={this.props.catName}
             height="261"
             width="261"
           />
         </a>
         <div className="details">
-          <div className="queer">
-            <p>#150:</p>
+          <div>
+            <p>{this.props.catNumber}</p>
             <h4>
-              <a href="https://octodex.github.com//octoqueer/">Octoqueer</a>
+              <a href={this.props.link}>{this.props.catName}</a>
             </h4>
           </div>
           <div className="wrapper">
-            <a href="https://github.com/cameronfoxly">
-              <img
-                src="https://github.com/cameronfoxly.png"
-                height="24"
-                width="24"
-              />
-            </a>
-            <a href="https://github.com/johncreek">
-              <img
-                src="https://github.com/johncreek.png"
-                height="24"
-                width="24"
-              />
-            </a>
-            <a href="https://github.com/tonyjaramillo">
-              <img
-                src="https://github.com/tonyjaramillo.png"
-                height="24"
-                width="24"
-              />
+            <a href={this.props.authorLink}>
+              <img src={this.props.authorImage} height="24" width="24" />
             </a>
           </div>
         </div>
